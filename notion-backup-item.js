b99c041b93bd952ec02442df98d4877862f0618a
Robin Berjon <robin.berjon@nytimes.com>
2021-05-03 17:47:19 -0400

    add ability to get a give item in JSON

---
+++ notion-backup-item.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+let { program } = require('commander')
+  , { writeFileSync } = require('fs')
+  , { join, isAbsolute } = require('path')
+  , { NotionAPI } = require('notion-client')
+  , { parsePageId } = require('notion-utils')
+  , { NOTION_TOKEN } = process.env
+  , nc = new NotionAPI({ authToken: NOTION_TOKEN })
+  , die = (str) => {
+      console.error(str);
+      process.exit(1);
+    }
+;
+
+// --version
+program.version(require('./package.json').version);
+
+program
+  .option('-i, --id <id>', 'ID of the Notion document')
+  .option('-c, --collection <collection>', 'ID of the Notion collection')
+  .option('-v, --view <view>', 'ID of the Notion collection view')
+  .option('-o, --out <file>', 'File to write to')
+;
+
+// now do something
+program.parse(process.argv);
+
+async function run () {
+  let { id, collection, view, out } = program.opts()
+    , refPage
+  ;
+  if (collection) {
+    if (!view) die('The --collection option requires --view to also be specified.');
+    if (id) console.warn('Warning: --id will be ignored.');
+    refPage = await nc.getCollectionData(parsePageId(collection), parsePageId(view));
+  }
+  else if (id) {
+    if (view) console.warn('Warning: --view will be ignored.');
+    refPage = await nc.getPage(parsePageId(id));
+  }
+  else die('Must specify one of --id or --collection/--view.');
+  let json = JSON.stringify(refPage, null, 2);
+  if (out) {
+    let file = isAbsolute(out) ? out : join(process.cwd(), out);
+    writeFileSync(file, json);
+  }
+  else process.stdout.write(json);
+}
+run();
